87042c388eb36f8a9b2faf007f5944cfd29acbc2
THARUN KUMAR <tharunkumar7041@gmail.com>
2025-09-02 00:45:36 +0530

    Rename project from "my_project" to "Rust_Practice" and refactor input functions for clarity

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io;
 use std::io::Write;
+use std::any::type_name;
 
-
-fn get_input() -> String {
+fn input() -> String {
     let mut input = String::new(); // Create a mutable String to store input
 
     print!("Enter : ");
@@ -12,12 +12,17 @@ fn get_input() -> String {
         .read_line(&mut input) // Read input from the user
         .expect("Failed to read line"); // Handle potential errors
 
-    println!("You've entered: {}", input.trim());
+    //println!("You've entered: {}", input.trim());
     input.trim().to_string()
 }
 
 
-fn input() -> String {
+fn type_of<T>(_: &T) -> &'static str {  // Remove & to accept values
+    type_name::<T>()
+}
+
+
+fn get_input() -> String {
     let mut input = String::new();
 
     println!("Enter : ");
@@ -29,6 +34,7 @@ fn input() -> String {
     input.trim().to_string()
 }
 
+
 fn main() {
   let _str = input();
   let s1 = String::from("Hello");
@@ -36,4 +42,10 @@ fn main() {
   let s3 = String::from("What a beautiful day!");
   let result = s1 + " " + &s2 + " " + &s3;
   println!("{}", result);
+
+
+
+  let age = 20;
+  let can_vote = age >= 18;
+  println!("Can vote? {}", type_of(&can_vote));
 }
