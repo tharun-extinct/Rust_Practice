3854b94575756b8d5d8615cbed96c6d565718030
THARUN KUMAR <tharunkumar7041@gmail.com>
2025-09-02 00:16:43 +0530

    Refactor input functions for clarity and consistency

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 use std::io;
+use std::io::Write;
 
-fn get_input() {
+
+fn get_input() -> String {
     let mut input = String::new(); // Create a mutable String to store input
 
     print!("Enter : ");
+    io::stdout().flush().unwrap();
 
     io::stdin()
         .read_line(&mut input) // Read input from the user
         .expect("Failed to read line"); // Handle potential errors
 
-    println!("You entered: {}", input.trim());
+    println!("You've entered: {}", input.trim());
+    input.trim().to_string()
 }
 
 
+fn input() -> String {
+    let mut input = String::new();
+
+    println!("Enter : ");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input.trim().to_string()
+}
+
 fn main() {
-  let str = get_input();
+  let _str = input();
   let s1 = String::from("Hello");
   let s2 = String::from("World!");
   let s3 = String::from("What a beautiful day!");
